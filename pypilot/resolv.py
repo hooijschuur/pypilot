3ed6cf48e5e6cd1d8d73bb091ea016461bb535de
march <march@march-To-be-filled-by-O-E-M>
2019-06-28 11:12:14 +0200

    PC-2019.1.3 <march@march-To-be-filled-by-O-E-M Overwrite remote https://github.com/hooijschuur/pypilot to local

--- pypilot/resolv.py
+++
@@ -1,6 +0,0 @@
-def resolv(angle, offset=0):
-    while offset - angle > 180:
-        angle += 360
-    while offset - angle <= -180:
-        angle -= 360
-    return angle
--- scripts/update_systemd_openplotter.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cp -rv debian/etc/systemd /etc
